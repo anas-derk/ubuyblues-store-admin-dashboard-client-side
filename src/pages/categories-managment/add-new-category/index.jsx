c45e6c699da9ccbc8c728ee6e5a447fbcc9bcf51
anas derk <anas.derk@gmail.com>
2026-01-31 19:15:34 +0300

    Test

--- src/pages/categories-managment/add-new-category/index.jsx
+++ src/pages/categories-managment/add-new-category/index.jsx
@@ -216,7 +216,7 @@ export default function AddNewCategory() {
                                 <ul className={`categories-list options-list bg-white border ${formValidationErrors["categoryParent"] ? "border-danger mb-4" : "border-dark"}`}>
                                     <li onClick={() => handleSelectCategoryParent("")}>No Parent</li>
                                     {searchedCategories.length > 0 && searchedCategories.map((category) => (
-                                        <li key={category._id} onClick={() => handleSelectCategoryParent(category)}>{category.name["en"]}</li>
+                                        <li key={category._id} onClick={() => handleSelectCategoryParent(category)}>{category.name["en"]} ({category?.parent ?? "No Parent"})</li>
                                     ))
                                     }
                                 </ul>
